cb43476b9df594413a754f7ef234a1c10086bd77
Kavyamol <74911379+Kavyamol@users.noreply.github.com>
2020-12-09 00:41:43 +0530

    Add files via upload

---
+++ BakedInHeaven.DataAccess/Repositories/IProductsRepository.cs
@@ -0,0 +1,11 @@
+﻿using BakedInHeaven.DataAccess.Entities;
+using System.Collections.Generic;
+
+namespace BakedInHeaven.DataAccess.Repositories
+{
+    public interface IProductsRepository
+    {
+        List<Products> GetAllProducts();
+        void Add(Products productEntity);
+    }
+}
